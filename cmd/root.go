b16b48b230f58430cf13cc45f5271dc8952b65ec
Minh Hoang Nguyen <983465+minhio@users.noreply.github.com>
2023-12-17 10:50:10 -0600

    update root command

--- cmd/root.go
+++ cmd/root.go
@@ -2,42 +2,42 @@ package cmd
 
 import (
 	"os"
+	"os/exec"
 
+	"github.com/loft-sh/log"
 	"github.com/spf13/cobra"
+	"golang.org/x/crypto/ssh"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "devpod-provider-multipass",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:           "devpod-provider-multipass",
+	Short:         "multipass provider commands",
+	SilenceErrors: true,
+	SilenceUsage:  true,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		log.Default.MakeRaw()
+		return nil
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	// execute command
 	err := rootCmd.Execute()
 	if err != nil {
-		os.Exit(1)
+		if exitErr, ok := err.(*ssh.ExitError); ok {
+			os.Exit(exitErr.ExitStatus())
+		}
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			if len(exitErr.Stderr) > 0 {
+				log.Default.ErrorStreamOnly().Error(string(exitErr.Stderr))
+			}
+			os.Exit(exitErr.ExitCode())
+		}
+
+		log.Default.Fatal(err)
 	}
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.devpod-provider-multipass.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }

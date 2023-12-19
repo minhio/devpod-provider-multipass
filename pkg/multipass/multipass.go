bbf5ef49d09647bc1530e1e1f0291938acb89fcc
Minh Hoang Nguyen <983465+minhio@users.noreply.github.com>
2023-12-18 23:59:51 -0600

    adsf

--- pkg/multipass/multipass.go
+++ pkg/multipass/multipass.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 
 	"github.com/minhio/devpod-provider-multipass/pkg/options"
 )
@@ -14,14 +15,14 @@ func Command() error {
 		return fmt.Errorf("command environment variable is missing")
 	}
 
-	multipassOptions, err := options.FromEnv()
+	opts, err := options.FromEnv()
 	if err != nil {
 		return err
 	}
 
-	machineId := multipassOptions.GetMachineId()
+	machineId := opts.GetMachineId()
 
-	cmd := exec.Command(multipassOptions.Path, "exec", machineId, "--", devPodCommand)
+	cmd := exec.Command(opts.Path, "exec", machineId, "--", devPodCommand)
 	cmd.Env = os.Environ()
 	err = cmd.Run()
 	if err != nil {
@@ -32,21 +33,57 @@ func Command() error {
 }
 
 func Create() error {
+	opts, err := options.FromEnv()
+	if err != nil {
+		return err
+	}
+
+	machineId := opts.GetMachineId()
+
+	cmd := exec.Command(opts.Path, "launch",
+		"--image", opts.Image,
+		"--cpus", strconv.Itoa(opts.Cpus),
+		"--disk", opts.DiskSize,
+		"--memory", opts.Memory,
+		"--name", machineId,
+		opts.Image,
+	)
+	cmd.Env = os.Environ()
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
+
 func Delete() error {
+	opts, err := options.FromEnv()
+	if err != nil {
+		return err
+	}
+
+	machineId := opts.GetMachineId()
+
+	cmd := exec.Command(opts.Path, "delete", machineId)
+	cmd.Env = os.Environ()
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func Init() error {
-	multipassOptions, err := options.FromEnv()
+	opts, err := options.FromEnv()
 	if err != nil {
 		return err
 	}
 
 	// execute 'multipass version' command
 	// as a way to check if multipass is available
-	cmd := exec.Command(multipassOptions.Path, "version")
+	cmd := exec.Command(opts.Path, "version")
 	cmd.Env = os.Environ()
 	err = cmd.Run()
 	if err != nil {
@@ -57,13 +94,55 @@ func Init() error {
 }
 
 func Start() error {
+	opts, err := options.FromEnv()
+	if err != nil {
+		return err
+	}
+
+	machineId := opts.GetMachineId()
+
+	cmd := exec.Command(opts.Path, "start", machineId)
+	cmd.Env = os.Environ()
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func Status() error {
+	opts, err := options.FromEnv()
+	if err != nil {
+		return err
+	}
+
+	machineId := opts.GetMachineId()
+
+	cmd := exec.Command(opts.Path, "info", machineId)
+	cmd.Env = os.Environ()
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func Stop() error {
+	opts, err := options.FromEnv()
+	if err != nil {
+		return err
+	}
+
+	machineId := opts.GetMachineId()
+
+	cmd := exec.Command(opts.Path, "stop", machineId)
+	cmd.Env = os.Environ()
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
